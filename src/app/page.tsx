df7685e4151b35001df42365924cf8b952ef178c
ASUS <you@example.com>
2025-11-20 20:37:41 +0200

    remove button

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -69,7 +69,7 @@ export default function HomePage() {
   // 4 Quick Links
   const quickLinks = [
     { title: 'التحقق من طلب جديد', link: '/verification' },
-    { title: 'أنتائج السابقة لمتقدم ثاني', link: '/archive' },
+    { title: 'نتائج السابقة لمتقدم ثاني', link: '/archive' },
     { title: 'الفيديوهات التعليمية', link: '/videos' },
     { title: 'المنح الدراسية', link: '/scholarships' },
   ];
